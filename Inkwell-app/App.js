a0e34da076c75025bcb6ef3aa6f43de90a1b6cd1
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-07-26 11:24:38 -0300

    Fear

--- Inkwell-app/App.js
+++ Inkwell-app/App.js
@@ -1,16 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/login_screen';
 import SearchScreen from './screens/search_screen';
 
-export default function App() {
-  return <SearchScreen/>;
-}
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen 
+          name="Search" 
+          component={SearchScreen} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
