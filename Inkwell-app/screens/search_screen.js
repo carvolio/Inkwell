e240aadbb3561ec046aa64d15b44b42b79cb6f68
guicaroliv <gui.car.dev@gmail.com>
2024-07-26 10:50:43 -0300

    epubjs test

--- Inkwell-app/screens/search_screen.js
+++ Inkwell-app/screens/search_screen.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import colors from '../constants.js';
+import { Reader, useReader } from '@epubjs-react-native/core';
+import { useFileSystem } from '@epubjs-react-native/file-system';
 
 export default function SearchScreen() {
   return (
-    <View style={styles.container}>
-      <Button onclick>
-        <Text>Search</Text>
-      </Button>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Reader
+        src="https://s3.amazonaws.com/moby-dick/OPS/package.opf"
+        fileSystem={useFileSystem}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.blue,
     alignItems: 'center',
     justifyContent: 'center',
   },
