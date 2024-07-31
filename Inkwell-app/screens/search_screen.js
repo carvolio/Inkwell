3828ef2ed680cbacd5f39b93bd135611ce76d560
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-07-31 11:17:22 -0300

    Reader test

--- Inkwell-app/screens/search_screen.js
+++ Inkwell-app/screens/search_screen.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { ReaderProvider, Reader, useReader } from '@epubjs-react-native/core';
+import { useFileSystem } from '@epubjs-react-native/expo-file-system';
 import colors from '../constants.js';
 
 export default function SearchScreen() {
+  const { goToLocation } = useReader();
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Search Screen</Text>
-    </SafeAreaView>
+    <ReaderProvider>
+      <SafeAreaView style={styles.container}>
+        <Reader
+          src="https://s3.amazonaws.com/moby-dick/OPS/package.opf"
+          fileSystem={useFileSystem}
+        />
+      </SafeAreaView>
+    </ReaderProvider>
   );
 }
 
