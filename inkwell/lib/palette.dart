2fd429d77c1a5f4a281b5b2d02250cb768f356b7
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-08-02 08:06:28 -0300

    init

---
+++ inkwell/lib/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const blue =  Color(0xFF355070);
+  static const magenta =  Color(0xFF6D597A);
+  static const salmon =  Color(0xFFB56576);
+  static const red =  Color(0xFFE56B6F);
+  static const orange =  Color(0xFFEAAC8B);
+}
